87fc4d2a6a0097bbeab0d3f3f27c50055f0c58d8
Srivatsa Srinath <srivatsa.srinath@gmail.com>
2022-02-16 14:12:37 +0530

    improving the typer options

--- mlserve/local_torchserve/model_server.py
+++ mlserve/local_torchserve/model_server.py
@@ -1,9 +1,30 @@
+from string import Template
 from pathlib import Path
 import subprocess, shlex
+from typing import Optional
+
+from mlserve.common.logger import logger
 
 import typer
 
 app = typer.Typer(name="Launch torchserve server", add_completion=False)
+
+# Edit config.properties
+def set_config_properties(
+    batch_size:int,
+    min_workers:int,
+    max_workers:Optional[int]=None
+    ) -> None:
+
+    replacements = {
+        "batch_size": str(batch_size),
+        "min_workers": str(min_workers),
+        'max_workers': str(max_workers or min_workers)
+    }
+    
+    orig_txt = Path("template_config.properties").read_text()
+    sub_txt = Template(orig_txt).substitute(replacements)
+    Path('config.properties').write_text(sub_txt, encoding='utf-8')
 
 #torch-model-archiver --model-name resnet18 \
 #--version 1.0 \
@@ -14,12 +35,21 @@ app = typer.Typer(name="Launch torchserve server", add_completion=False)
 @app.command()
 def regen_mar(
     model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
-    model_pth:str=typer.Option('../models/resnet18.pth', "-mm", help="Model .pth file"),
-    extra_files:str=typer.Option('./FVHandler.py',"-ef", help="Extra Files"),
-    handler:str=typer.Option('./serve_handle.py', "-hd", help="Handler function"),
+    model_info_dir:Path=typer.Option("model_info", "-mm", help="Model .pth file", exists=True),
+    extra_files:str=typer.Option('FVHandler.py',"-ef", help="Extra Files"),
+    handler:str=typer.Option('serve_handle.py', "-hd", help="Handler function"),
     model_store:str=typer.Option('model_store', "-ms", help="Model Store location"),
-) -> None:
+) -> str:
     
+    model_pth = f"{model_info_dir}/{model_name}/{model_name}.pth"
+    if not Path(model_pth).exists():
+        typer.echo(f"{model_pth} does not exists")
+        typer.Exit()
+        
+    # Currently support for a single file
+    extra_files = f"{model_info_dir}/{model_name}/{extra_files}"
+    handler = f"{model_info_dir}/{model_name}/{handler}"
+
     if not Path(model_store).exists():
         Path(model_store).mkdir(exist_ok=True)
 
@@ -37,12 +67,14 @@ def regen_mar(
         raise typer.Exit()
     else:
         typer.echo(f"Successfully created : {mar_file.resolve().__str__()}")
+        
+    return mar_file.resolve().__str__()
 
 #torchserve --start --foreground --model-store model_store/ \
 # --models resnet18=resnet18.mar --ts-config config.properties
 @app.command()
-def launch_server(
-model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
+def launch_local_server(
+    model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
     model_mar:str=typer.Option('resnet18.mar', "-mm", help="Model .mar file"),
     model_store:str=typer.Option('model_store', "-ms", help="Model Store location"),
     ts_config_file:str=typer.Option('config.properties', "-ts", help="TS config file")
@@ -68,14 +100,13 @@ model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
 # --models resnet18=resnet18.mar --ts-config config.properties
 @app.command()
 def launch_docker_server(
-model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
-    model_mar:str=typer.Option('resnet18.mar', "-mm", help="Model .mar file"),
+    model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
     model_store:str=typer.Option('model_store', "-ms", help="Model Store location"),
     ts_config_file:str=typer.Option('config.properties', "-ts", help="TS config file"),
     docker_image:str=typer.Option('pytorch/torchserve:latest-cpu', "-di", help="docker image")
 ) -> None:
     subprocess.run(shlex.split("rm -rf ./logs"))
-    mar_file = Path(f"{model_store}/{model_mar}")
+    mar_file = Path(f"{model_store}/{model_name}.mar")
     
     cwd = Path('.').resolve().__str__()
 
@@ -87,7 +118,7 @@ model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
 
     launch_cmd = docker_cmd + f"torchserve --start --foreground \
                     --model-store /home/model-server/{model_store} \
-                    --models {model_name}={model_mar} \
+                    --models {model_name}={mar_file.name} \
                     --ts-config /home/model-server/{ts_config_file}"
                     
     if mar_file.exists():
@@ -99,24 +130,43 @@ model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
 @app.command()
 def regen_launch(
     model_name:str=typer.Option('resnet18', "-mn", help="Model name"),
-    model_pth:str=typer.Option('../models/resnet18.pth', "-mm", help="Model .pth file"),
-    model_mar:str=typer.Option('resnet18.mar', "-mm", help="Model .mar file"),
-    extra_files:str=typer.Option('./FVHandler.py',"-ef", help="Extra Files"),
-    handler:str=typer.Option('./serve_handle.py', "-hd", help="Handler function"),
+    model_info_dir:Path=typer.Option('./model_info', "-mm", help="Model .pth directory"),
+    extra_files:str=typer.Option('FVHandler.py',"-ef", help="Extra Files"),
+    handler:str=typer.Option('serve_handle.py', "-hd", help="Handler function"),
     model_store:str=typer.Option('model_store', "-ms", help="Model Store location"),
-    ts_config_file:str=typer.Option('config.properties', "-ts", help="TS config file")
+    ts_config_file:Optional[str]=typer.Option(None, "-ts", help="TS config file"),
+    batch_size:int=typer.Option(1, "-bs", help="batch size"),
+    min_workers:int=typer.Option(1, "-minw", help="minimum workers"),
+    max_workers:int=typer.Option(None, "-maxw", help="maximum workers")
 ) -> None:
-    regen_mar(
+
+    mar_file_path = regen_mar(
         model_name=model_name,
-        model_pth=model_pth,
+        model_info_dir=model_info_dir,
         model_store=model_store,
         extra_files=extra_files,
         handler=handler
     )
     
+    model_mar = f"{model_name}.mar"
+    if not Path(f"{model_store}/{model_mar}").exists():
+        typer.echo(f"Missing {model_mar} in {model_store} directory")
+        typer.Exit()
+    
+    if ts_config_file is None:
+        set_config_properties(
+            batch_size=batch_size,
+            min_workers=min_workers,
+            max_workers=max_workers or min_workers
+        )
+        ts_config_file = 'config.properties'
+    else:
+        ts_config_file = ts_config_file.name
+        
+    logger.info(f"Config file used: {ts_config_file}")
+
     launch_docker_server(
         model_name=model_name,
-        model_mar=model_mar,
         model_store=model_store,
         ts_config_file=ts_config_file
     )
